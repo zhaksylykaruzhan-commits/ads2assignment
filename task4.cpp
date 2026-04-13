b4613a687de63814547403c1f8f6f8e4435cc560
Zhaksylyk Aruzhan -2511! <zhaksylyk.aruzhan.alash@gmail.com>
2026-04-13 13:55:10 +0500

    Add task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    string queue[100];
+    int front = 0, rear = 0;
+
+    queue[rear++] = "1";
+
+    for (int i = 0; i < n; i++) {
+        string curr = queue[front++];
+        cout << curr << " ";
+
+        queue[rear++] = curr + "0";
+        queue[rear++] = curr + "1";
+    }
+
+    return 0;
+}
