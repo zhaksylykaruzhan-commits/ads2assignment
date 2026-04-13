ea14e6a96ec710c7cdb669f7c0fc30c716a2d4a1
Zhaksylyk Aruzhan -2511! <zhaksylyk.aruzhan.alash@gmail.com>
2026-04-13 13:54:33 +0500

    Update  task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char s[] = "{[()]}";
+    char stack[100];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char ch = s[i];
+
+        if (ch == '(' || ch == '{' || ch == '[') {
+            stack[++top] = ch;
+        } else {
+            if (top == -1) {
+                cout << "Invalid";
+                return 0;
+            }
+
+            char last = stack[top--];
+
+            if ((ch == ')' && last != '(') ||
+                (ch == '}' && last != '{') ||
+                (ch == ']' && last != '[')) {
+                cout << "Invalid";
+                return 0;
+                }
+        }
+    }
+
+    if (top == -1)
+        cout << "Valid";
+    else
+        cout << "Invalid";
+
+    return 0;
+}
