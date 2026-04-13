33bae03abe795120d4ed5ae5f4c13faa0128ac01
Zhaksylyk Aruzhan -2511! <zhaksylyk.aruzhan.alash@gmail.com>
2026-04-13 13:53:34 +0500

    add task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    Node* next;
+};
+
+bool hasCycle ( Node* head) {
+    Node* slow = head;
+   const Node* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
